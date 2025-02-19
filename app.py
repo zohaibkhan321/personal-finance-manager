3869edaa46e12bd05e496556847399efe4191c8f
Muhammad Zohaib Imtiaz <117985032+zohaibkhan321@users.noreply.github.com>
2025-02-19 15:59:27 +0500

    hotfix

--- app.py
+++ app.py
@@ -4,13 +4,6 @@ import plotly.express as px
 import datetime
 import os
 
-# --- Load Custom CSS from assets/style.css ---
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-css_file = os.path.join("assets", "style.css")
-local_css(css_file)
 
 # --- App Title ---
 st.title("💰 Personal Finance Manager")
